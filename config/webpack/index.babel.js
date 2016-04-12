690972999659f5f51cb22abd95c70093d9083734
Ian MacLeod <ian@nevir.net>
2016-04-11 21:00:10 -0700

    Link to webpack docs, and organize config to follow it

--- config/webpack/index.babel.js
+++ config/webpack/index.babel.js
@@ -5,8 +5,13 @@ import HtmlPlugin from 'html-webpack-plugin';
 
 import { current as config } from '..';
 
-export const devtool = 'cheap-module-eval-source-map';
+// https://webpack.github.io/docs/configuration.html#output
+export const output = {
+  path: path.join(config.buildDir, config.variant),
+  filename: 'app.js',
+};
 
+// https://webpack.github.io/docs/configuration.html#entry
 export const entry = {
   vendor: _.compact([
     config.watch && 'webpack-hot-middleware/client',
@@ -17,25 +22,7 @@ export const entry = {
   ],
 };
 
-export const output = {
-  path: path.join(config.buildDir, config.variant),
-  filename: 'app.js',
-};
-
-export const plugins = _.compact([
-  new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
-  config.watch && new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoErrorsPlugin(),
-  new HtmlPlugin({
-    inject: false,
-    template: path.join(config.srcDir, 'index.html.ejs'),
-    minify: {
-      removeComments: true,
-      collapseWhitespace: true,
-    },
-  }),
-]);
-
+// https://webpack.github.io/docs/configuration.html#module
 export const module = {
   loaders: [
     {
@@ -50,6 +37,25 @@ export const module = {
   ],
 };
 
+// https://webpack.github.io/docs/configuration.html#resolve
 export const resolve = {
   extensions: ['', '.jsx', '.js'],
 };
+
+// https://webpack.github.io/docs/configuration.html#plugins
+export const plugins = _.compact([
+  new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+  config.watch && new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin(),
+  new HtmlPlugin({
+    inject: false,
+    template: path.join(config.srcDir, 'index.html.ejs'),
+    minify: {
+      removeComments: true,
+      collapseWhitespace: true,
+    },
+  }),
+]);
+
+// https://webpack.github.io/docs/configuration.html#devtool
+export const devtool = 'cheap-module-eval-source-map';
