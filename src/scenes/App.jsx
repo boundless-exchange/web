028ac346cb24890173633301e421dc41b460a71a
Ian MacLeod <ian@nevir.net>
2016-04-13 22:23:39 -0700

    Let's switch back to stateless components where possible

--- src/scenes/App.jsx
+++ src/scenes/App.jsx
@@ -1,9 +1,10 @@
-import { BaseComponent, Header } from '../components';
+import { Header } from '../components';
 
-export default class App extends BaseComponent {
-
-  render() {
-    return <Header>Hi</Header>;
-  }
-
+export default function App({children}) {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
 }
