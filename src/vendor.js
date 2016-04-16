60c41adff4b8d61b7f5e48ff7087bd5f8c8c9de0
Ian MacLeod <ian@nevir.net>
2016-04-16 12:56:21 -0700

    'hot' reloading for vendor.js

--- src/vendor.js
+++ src/vendor.js
@@ -7,3 +7,12 @@ import * as React from 'react';
 
 // Make React global so that we can use JSX without fear.
 global.React = React;
+
+if (module.hot) {
+  module.hot.accept();
+  module.hot.dispose(() => {
+    // Vendor code is global; we can't expect everyone downstream to refresh all
+    // their references.
+    window.location.reload();
+  });
+}
