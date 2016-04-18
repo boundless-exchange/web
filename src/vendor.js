0e91a8db6802dee5c969f1bd24d342f6378a1ec8
Ian MacLeod <ian@nevir.net>
2016-04-17 23:14:01 -0700

    Fill in vendor.js

--- src/vendor.js
+++ src/vendor.js
@@ -1,9 +1,18 @@
 import 'babel-polyfill';
+import 'chroma-js';
+import 'classlist-polyfill';
+import 'combined-reduction';
 import 'lodash';
 import 'normalize.css';
 import 'react-addons-shallow-compare';
 import 'react-dom';
 import 'react-look';
+import 'react-router-redux';
+import 'react-router';
+import 'redux-interactions';
+import 'redux-thunk';
+import 'react-redux';
+import 'redux';
 import * as React from 'react';
 
 // Make React global so that we can use JSX without fear.
