fc3cf57a8001a9572db232ac96722e9e61795fb8
Ian MacLeod <ian@nevir.net>
2016-04-19 23:01:38 -0700

    World builder prefix

--- src/components/Layout/Navigation.jsx
+++ src/components/Layout/Navigation.jsx
@@ -1,16 +1,25 @@
+import chroma from 'chroma-js';
 import { StyleSheet } from 'react-look';
 
 import { BaseComponent } from '..';
+import { colors, fonts, sizes, styles } from '../../constants';
 import NavigationLink from './NavigationLink';
 
 const STYLES = StyleSheet.create({
+  worldBuilder: {
+    ...fonts.HEADER.MEDIUM,
+    display: 'inline-block',
+    color: chroma(colors.MONOCHROME.LIGHT).alpha(0.5).css(),
+    padding: sizes.SPACING.SMALL,
+  },
 });
 
 export default class Navigation extends BaseComponent {
 
   render() {
     return (
-      <div className={`${STYLES.root} ${this.props.className}`}>
+      <div className={this.props.className}>
+        <div className={STYLES.worldBuilder}>World Builder:</div>
         <NavigationLink path='/world-builder/guide'>Guide</NavigationLink>
         <NavigationLink path='/world-builder/reference'>Reference</NavigationLink>
         <NavigationLink path='/world-builder/techniques'>Techniques</NavigationLink>
