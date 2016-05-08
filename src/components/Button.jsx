a01de39c5c2f1a15c7ec0c9b4115b54010689df3
Ian MacLeod <ian@nevir.net>
2016-05-07 21:56:44 -0700

    Display active route

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,7 @@
 import * as _ from 'lodash';
+import chroma from 'chroma-js';
 import look, { StyleSheet } from 'react-look';
+import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { PropTypes } from 'react';
 
@@ -36,13 +38,21 @@ const STYLES = StyleSheet.create({
   },
   content: {
     ...fonts.compactHeader(fonts.SIZES.COPY),
-    transition: `transform ${animation.DEFAULT}, background ${animation.DEFAULT}`,
+    transition: `
+      transform ${animation.DEFAULT},
+      background ${animation.DEFAULT},
+      border-right-color ${animation.DEFAULT}
+    `,
     transform: `translateZ(${sizes.DEPTH}px)`,
     transformStyle: 'preserve-3d',
     backgroundColor: colors.DIALOG.BACKGROUND,
+    borderRight: `${sizes.BORDER.THICK}px solid ${chroma(colors.ACCENT).alpha(0).css()}`,
     padding: sizes.SPACING.SMALL,
     cursor: 'pointer',
     userSelect: 'none',
+    'active=true': {
+      borderRight: `${sizes.BORDER.THICK}px solid ${colors.ACCENT}`,
+    },
     'highlight=true': {
       backgroundColor: colors.DIALOG.BACKGROUND_HIGHLIGHT,
     },
@@ -53,6 +63,7 @@ const STYLES = StyleSheet.create({
   },
 });
 
+@connect((_state, _props) => ({}))
 @look
 export default class Button extends BaseComponent {
 
@@ -64,6 +75,27 @@ export default class Button extends BaseComponent {
   static defaultProps = {
     depth: 1,
   };
+
+  static contextTypes = {
+    router: PropTypes.shape({
+      isActive: PropTypes.func.isRequired,
+    }).isRequired,
+  }
+
+  constructor(props, context, ...args) {
+    super(props, context, ...args);
+
+    this.state = {
+      active: context.router.isActive(props.to),
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // react-look actively fights react-router's Link for control of className.
+    //
+    // So, we manage it ourselves instead.
+    this.setState({active: this.context.router.isActive(nextProps.to)});
+  }
 
   render() {
     return (
