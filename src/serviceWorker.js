9a56ac97051cc853078d3ff74d0870f97d7c94ec
Ian MacLeod <ian@nevir.net>
2016-05-07 21:15:49 -0700

    An attempt at a long-lived, but refreshable, cache

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,2 +1,37 @@
 // THIS FILE IS NOT COMPILED BY BABEL.
-console.log('in service worker');
+const CACHE_NAME = 'v1';
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+
+  event.respondWith(
+    caches.match(request)
+      .then(response => {
+        if (response) return response;
+
+        if (isHtmlRequest(request)) {
+          return caches.match('/')
+            .then(htmlResponse => {
+              if (htmlResponse) return htmlResponse;
+              return fetchAndCache('/', {cache: 'force-cache'});
+            });
+        } else {
+          return fetchAndCache(request);
+        }
+      })
+  );
+});
+
+function fetchAndCache(request, init) {
+  return fetch(request, init)
+    .then(response => {
+      caches.open(CACHE_NAME).then(cache => {
+        cache.put(request, response);
+      });
+      return response.clone();
+    });
+}
+
+function isHtmlRequest(request) {
+  return new URL(request.url).pathname.indexOf('.') === -1;
+}
