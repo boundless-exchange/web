b74b0058b3eeabb81626c438682926b238a95218
Ian MacLeod <ian@nevir.net>
2016-06-04 13:47:18 -0700

    Fill in a stub for techniques

--- src/articles/world-builder/index.js
+++ src/articles/world-builder/index.js
@@ -23,6 +23,10 @@ export const reference = {
 export const techniques = {
   title: `Techniques`,
   load() {
-    return Promise.reject(new Error(`Not implemented yet`));
+    return new Promise((resolve, _reject) => {
+      require.ensure([], require => {
+        resolve(require('./techniques').default);
+      }, 'world-builder/techniques');
+    });
   },
 };
---
+++ src/articles/world-builder/techniques/intro.md
@@ -0,0 +1,4 @@
+---
+title: World Builder Techniques
+notice: stub
+---
